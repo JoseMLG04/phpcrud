7ee58f2835fac80f070cb3006bec8c7232b1af0c
LiquidSebabas <sebabasvevo@outlook.com>
2024-09-17 21:06:52 -0600

    CSS

--- modificar.php
+++ modificar.php
@@ -40,7 +40,6 @@
                     </div>
                 </div>
 
-                <!-- Bootstrap JS (Opcional, pero útil para interacción como modales o tooltips) -->
                 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
             </body>
             </html>
